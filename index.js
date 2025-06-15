d3045ca3a41245c5fcca4f00ae492670d9b5508c
muuo <anthonymuuo92@gmail.com>
2025-06-15 12:04:27 +0300

    finished the posts requests endpoints

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 const client = new PrismaClient();
 
+app.get("/", (_req, res) => {
+  res.send("<h3>Welcome to the Blogging Api endpoint</h3>");
+});
+
 //get all users
 app.get("/users", async (_req, res) => {
   try {
@@ -44,6 +48,89 @@ app.post("/users", async (req, res) => {
     res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json("error creating user");
+  }
+});
+
+//posts
+
+app.get("/posts", async (req, res) => {
+  try {
+    const allPosts = await client.posts.findMany({
+      where: { isDeleted: false },
+    });
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(500).json("error fetching all posts");
+  }
+});
+
+//creating a posts
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { title, content, userId } = req.body;
+    const newPost = await client.posts.create({
+      data: {
+        title,
+        content,
+        userId,
+      },
+    });
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(500).json("error creating all posts");
+  }
+});
+
+//getting specific post
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getPost = await client.posts.findUnique({
+      where: { id },
+    });
+    res.status(200).json(getPost);
+  } catch (error) {
+    res.status(500).json("error fetching specific user");
+  }
+});
+
+//updating a specific Post
+
+app.put("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res
+        .status(400)
+        .json("Both title and content are required for update request");
+    }
+    const updatedPost = await client.posts.update({
+      where: { id },
+      data: {
+        title,
+        content,
+      },
+    });
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json("error updating this post");
+  }
+});
+
+//Delete a given post
+app.patch("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const removed = await client.posts.update({
+      where: { id },
+      data: { isDeleted: true },
+    });
+    res.status(200).json(removed);
+  } catch (error) {
+    res.status(500).json("error deleting this specific post");
   }
 });
 
---
+++ prisma/migrations/20250614151817_set_default/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "posts_table" ALTER COLUMN "posts_deleted" SET DEFAULT false;
